8de47b12526c7d6dd10cbf2f00265890946d3f7f
Martin Buhr <martin@tyk.io>
2015-06-20 07:50:29 -0400

    Hybrid setup

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,11 @@ sed -i 's/PORT/'$PORT'/g' /opt/tyk/tyk.conf
 sed -i 's/SECRET/'$SECRET'/g' /opt/tyk/tyk.conf
 sed -i 's/ORGID/'$ORGID'/g' /opt/tyk/tyk.conf
 sed -i 's/APIKEY/'$APIKEY'/g' /opt/tyk/tyk.conf
+
+cat /opt/tyk/tyk.conf
+
 cd /opt/tyk/
+
+service redis-server start
 
 ./tyk --conf=tyk.conf
--- start.sh
+++ start.sh
@@ -9,4 +9,4 @@ sudo docker stop tyk_hybrid && docker rm tyk_hybrid
 
 sudo docker pull tykio/tyk-hybrid-docker
 
-docker run -d --name tyk_hybrid -p $PORT:$PORT -e SECRET=$SECRET -e ORGID=$ORGID -e APIKEY=$APIKEY tykio/tyk-hybrid-docker
+docker run -d --name tyk_hybrid -p $PORT:$PORT -e PORT=$PORT -e SECRET=$SECRET -e ORGID=$ORGID -e APIKEY=$APIKEY tykio/tyk-hybrid-docker
