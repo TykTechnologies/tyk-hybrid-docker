8c78ef45e7b126a92b4bf097b9b134824313c596
Martin Buhr <martin@tyk.io>
2015-08-24 10:37:48 -0400

    Redis port and host now configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,6 +4,8 @@ sed -i 's/PORT/'$PORT'/g' /opt/tyk/tyk.conf
 sed -i 's/SECRET/'$SECRET'/g' /opt/tyk/tyk.conf
 sed -i 's/ORGID/'$ORGID'/g' /opt/tyk/tyk.conf
 sed -i 's/APIKEY/'$APIKEY'/g' /opt/tyk/tyk.conf
+sed -i 's/REDIS/'$REDIS'/g' /opt/tyk/tyk.conf
+sed -i 's/RPORT/'$RPORT'/g' /opt/tyk/tyk.conf
 
 cd /opt/tyk/
 
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-USAGE="--> Usage: ./start.sh PORT SECRET ORGID APIKEY"
+USAGE="--> Usage: ./start.sh PORT SECRET ORGID APIKEY (REDIS HOST) (REDIS PORT)"
 
 if [ -z "$1" ]
 then
@@ -29,6 +29,20 @@ then
         exit
 fi
 
+REDIS=$5
+if [ -z "$5" ]
+then
+        echo "Using Redis localhost"
+        REDIS=localhost
+fi
+
+RPORT=$6
+if [ -z "$6" ]
+then
+        echo "Using Redis default port"
+        RPORT=6379
+fi
+
 PORT=$1
 SECRET=$2
 ORGID=$3
@@ -40,7 +54,7 @@ sudo docker stop tyk_hybrid && docker rm tyk_hybrid
 
 sudo docker pull tykio/tyk-hybrid-docker
 
-docker run -v $cwd/confs:/etc/nginx/sites-enabled -d --name tyk_hybrid -p $PORT:$PORT -p 80:80 -e PORT=$PORT -e SECRET=$SECRET -e ORGID=$ORGID -e APIKEY=$APIKEY tykio/tyk-hybrid-docker
+docker run -v $cwd/confs:/etc/nginx/sites-enabled -d --name tyk_hybrid -p $PORT:$PORT -p 80:80 -e PORT=$PORT -e SECRET=$SECRET -e ORGID=$ORGID -e APIKEY=$APIKEY -e REDIS=$REDIS -e RPORT=$RPORT tykio/tyk-hybrid-docker
 
 echo "Tyk Hybrid Node Running"
 echo "- To set up domains, place nginx server configs into the confs/ folder and restart"
