a2cb24d26d2d7d66cba1e25c44c563c49a687ec3
Martin Buhr <martin@tyk.io>
2016-02-29 10:12:16 -0500

    Check PW

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,13 @@ sed -i 's/ORGID/'$ORGID'/g' /opt/tyk/tyk.conf
 sed -i 's/APIKEY/'$APIKEY'/g' /opt/tyk/tyk.conf
 sed -i 's/REDISHOST/'$REDISHOST'/g' /opt/tyk/tyk.conf
 
+if [ -z "$VAR" ]
+	then
+		sed -i 's/REDISPW/'""'/g' /opt/tyk/tyk.conf
+	else
+		sed -i 's/REDISPW/'$REDISPW'/g' /opt/tyk/tyk.conf
+fi
+
 cd /opt/tyk/
 
 service redis-server start
